f1080379f76ebb717d5917340f959b18a3802f54
SteveCutler <stephen.c.cutler@gmail.com>
2024-01-19 13:24:13 -0500

    Initial Commit

---
+++ SpotMultitool2 Watch App/SpotMultitool2App.swift
@@ -0,0 +1,17 @@
+//
+//  SpotMultitool2App.swift
+//  SpotMultitool2 Watch App
+//
+//  Created by Steve Cutler on 2024-01-19.
+//
+
+import SwiftUI
+
+@main
+struct SpotMultitool2_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpotMultitool2/SpotMultitool2App.swift
@@ -0,0 +1,17 @@
+//
+//  SpotMultitool2App.swift
+//  SpotMultitool2
+//
+//  Created by Steve Cutler on 2024-01-19.
+//
+
+import SwiftUI
+
+@main
+struct SpotMultitool2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
